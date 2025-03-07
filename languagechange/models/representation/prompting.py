c46c562bf86a68e998cf953295fd149c7504e131
ErikKjellberg <erik.kjellberg.kolterjahn@gmail.com>
2025-03-07 10:31:11 +0100

    Added a preliminary PromptModel class using the LangChain environment

--- languagechange/models/representation/prompting.py
+++ languagechange/models/representation/prompting.py
@@ -1,23 +1,60 @@
 from typing import Tuple, List, Union, Any
 from languagechange.usages import TargetUsage
-from openai import OpenAI
+import getpass
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
 import logging
-import re
 import trankit
 
-# Generative model, should be compatible both with ChatGPT and DeepInfra.
+
+class SCFloat(BaseModel):
+    change : float = Field(description='The semantic change on a scale from 0 to 1.',le=1, ge=0)
+
+
+class SCDURel(BaseModel):
+    change : int = Field(description='The semantic similary from 1 to 4, where 1 is unrelated, 2 is distantly related, 3 is closely related and 4 is identical.',le=4, ge=1)
+
+
 class PromptModel:
-    def __init__(self, model_name : str, key, base_url : str = None):
+    def __init__(self, model_name : str, model_provider : str, langsmith_key : str = None, provider_key : str = None, scale="float"):
         self.model_name = model_name
-        if not base_url:
-            self.client = OpenAI(api_key = key)
+
+        os.environ["LANGSMITH_TRACING"] = "true"
+        
+        # The keys can either be passed as arguments, stored as an environment variable or put in manually
+        if langsmith_key != None:
+            os.environ["LANGSMITH_API_KEY"] = langsmith_key
+        elif not os.environ.get("LANGSMITH_API_KEY"):
+            os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter API key for LangSmith: ")
+
+        provider_key_name = {'openai':"OPENAI_API_KEY", "groq":"GROQ_API_KEY"}[model_provider]
+        if provider_key != None:
+            os.environ[provider_key_name] = provider_key
+        elif not os.environ.get(provider_key_name):
+            os.environ[provider_key_name] = getpass.getpass(f"Enter API key for {model_provider}: ")
+
+        llm = init_chat_model(model_name, model_provider=model_provider)
+
+        if scale == "float":
+            self.structure = SCFloat
+        elif scale == "DURel":
+            self.structure = SCDURel
+        # Todo: add support for custom measurements
         else:
-            self.client = OpenAI(api_key = key, base_url=base_url) # For DeepInfra use "https://api.deepinfra.com/v1/openai"
+            self.structure = None
+
+        if self.structure != None:
+            self.model = llm.with_structured_output(self.structure)
+        else:
+            self.model = llm
 
 
     def get_response(self, target_usages : List[TargetUsage], 
-                     system_message = 'You are a lexicographer',
-                     prompt_template = 'Please provide a single number between 0 and 1 measuring how different the meaning of the word \'{}\' is between the following example sentences: \n1. {}\n2. {}'):
+                     system_message : str = 'You are a lexicographer',
+                     user_prompt_template : str = 'Please provide a number measuring how different the meaning of the word \'{target}\' is between the following example sentences: \n1. {usage_1}\n2. {usage_2}',
+                     lemmatize : bool = True):
         
         assert len(target_usages) == 2
 
@@ -32,42 +69,32 @@ class PromptModel:
                 if token['span'] == tuple(usage.offsets):
                     return(token['lemma'])
                 
-        p = trankit.Pipeline("english")
-        lemmatized = [p.lemmatize(sentence, is_sent = True) for sentence in sentences]
-        lemmas = [get_lemma(lemmatized[i], target_usages[i]) for i in range(2)]
-        
-        if lemmas[0] != lemmas[1]:
-            logging.info("Lemmas of the two words differ, are you sure they are the same?")
-        lemma = lemmas[0]
+        if lemmatize:
+            p = trankit.Pipeline("english")
+            lemmatized = [p.lemmatize(sentence, is_sent = True) for sentence in sentences]
+            lemmas = [get_lemma(lemmatized[i], target_usages[i]) for i in range(2)]
             
-        prompt = prompt_template.format(lemma, sentences[0], sentences[1])
+            if lemmas[0] != lemmas[1]:
+                logging.info("Lemmas of the two words differ, are you sure they are the same?")
+            target = lemmas[0]
+        else:
+            target = words[0]
 
-        logging.info(f"Prompt: {prompt}")
-        print(prompt)
+        prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_message), ("user", user_prompt_template)]
+        )
+            
+        prompt = prompt_template.invoke({"target": target, "usage_1": sentences[0], "usage_2": sentences[1]})
         
         try:
-            completion = self.client.chat.completions.create(
-            model=self.model_name,
-            messages=[
-                {"role": "developer", "content": system_message},
-                {"role": "user", "content": prompt}
-            ]
-            )
-
-            message = completion.choices[0].message()
+            response = self.model.invoke(prompt)
         except:
             logging.info("Could not run chat completion.")
             return None
-
-        # Extract the numbers from the answer of the chat model
-        numbers = []
-        for match in re.finditer('\s\d(\.\d+)?',message):
-            number = message[match.start():match.end()]
-            try:
-                if 0 <= float(number) <= 1:
-                    numbers.append(float(number))
-            except:
-                continue
-        # Assuming the model's answer will be the first number it mentions
-        return numbers[0]
         
+        try:
+            # Structured output
+            return response.change
+        except:
+            # Non-structured output
+            return response
